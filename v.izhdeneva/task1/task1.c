9fbc56df3b9b7dc097d0a47e99489a56ae276dd8
izhdenevav <vizdeneva@gmail.com>
2024-10-15 12:14:05 +0700

    edit tasks

--- v.izhdeneva/task1/task1.c
+++ v.izhdeneva/task1/task1.c
@@ -7,6 +7,8 @@
 #include <sys/resource.h>
 #include <sys/time.h>
 
+extern char **environ;
+
 void print_id() {
     uid_t uid = getuid(); // реальный айди юзера
     uid_t euid = geteuid(); // эффективный айди юзера
@@ -43,8 +45,10 @@ void print_cwd() {
     printf("CWD: %s\n", path);
 }
 
-void print_env_vars(char* envp[]) {
-    for (int i = 0; envp[i]; i++) printf("%s\n", envp[i]);
+void print_env_vars() {
+    printf("environment variables are:\n");
+    for (char **p = environ; *p; p++) printf("%s\n", *p);
+
 }
 
 void get_ulimit_value() {
@@ -73,7 +77,7 @@ void set_env_ver(char* var) {
     putenv(var);
 }
 
-int main(int argc, char *argv[], char *envp[]) {
+int main(int argc, char *argv[]) {
     int opt;
     while ((opt = getopt(argc, argv, "ispdvucU:C:V:")) != -1) {
         switch (opt) {
@@ -90,7 +94,7 @@ int main(int argc, char *argv[], char *envp[]) {
                 print_cwd();
                 break;
             case 'v':
-                print_env_vars(envp);
+                print_env_vars();
                 break;
             case 'u':
                 get_ulimit_value();
