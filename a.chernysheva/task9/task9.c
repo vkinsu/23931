4491f75f12594b0839b5b0b3a83ce108ee37c964
Arina Chernysheva <a.chernysheva1@g.nsu.ru>
2024-11-11 20:13:58 +0700

    task_9

---
+++ a.chernysheva/task9/task9.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <spawn.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+int main(int argv, char* argc[]) {
+    if (argv == 1) {
+        printf("No filename.\n");
+        return 1;
+    }
+    printf("Parent pid is: %i\n", getpid());
+    char* args_to_child[] = { "cat", argc[1], NULL };
+    pid_t child;
+
+    child = fork();
+    if (child == (-1)) {
+        printf("Error in child process creating.");
+        return 1;
+    }
+    else if (!child) {
+        printf("Child process has been created with pid: %i\n", getpid());
+        execv("/bin/cat", args_to_child);//каталог содержащий исполняемые файлы
+        printf("Finish child process.\n");
+        exit(0);
+    }
+    else if(child) {
+        printf("\nWait for the child\n");
+        wait(NULL);
+        printf("\nChild process is ended.\n");
+    }
+    return 0;
+}
