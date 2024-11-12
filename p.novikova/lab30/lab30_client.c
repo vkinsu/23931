f659563516a00cff6517bdb418b7f95c4c27b29f
Polina Novikova <p.novikova@g.nsu.ru>
2024-11-12 17:39:02 +0700

    lab30

---
+++ p.novikova/lab30/lab30_client.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/uppercase_socket"
+
+int main() {
+    int client_fd;
+    struct sockaddr_un server_addr;
+
+    // Create socket
+    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client_fd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up server address structure
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+
+    // Connect to the server
+    if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send data to the server
+    char message[] = "Hello, server!";
+    send(client_fd, message, sizeof(message), 0);
+
+    // Close the socket
+    close(client_fd);
+
+    return 0;
+}
