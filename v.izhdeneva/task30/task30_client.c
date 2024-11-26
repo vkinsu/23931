90ddb13760afa6e565826e0efa4b66e88abef424
izhdenevav <vizdeneva@gmail.com>
2024-11-26 09:20:30 +0700

    task30

---
+++ v.izhdeneva/task30/task30_client.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "./unix_domain_socket"
+
+int main() {
+    char server_message[256] = "You have reached the server!";
+
+    // create the server socket
+    int client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    // check if we can't create the socket
+    if (client_socket == -1) {
+        perror("\nxxxx socket xxxx\n");
+        exit(-1);
+    }
+
+    // define the server address
+    struct sockaddr_un server_address;
+    server_address.sun_family = AF_UNIX;
+    strncpy(server_address.sun_path, SOCKET_PATH, sizeof(server_address.sun_path) - 1);
+    // server_address.sun_port = htons(PORT);
+    // server_address.sun_addr.s_addr = INADDR_ANY;
+
+    int connect_socket = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (connect_socket == -1) {
+        perror("\nxxxx connect xxxx\n");
+        exit(-1);
+    }
+    
+    int send_data = send(client_socket, server_message, strlen(server_message), 0);
+    if (send_data == -1) {
+        perror("\nxxxx send xxxx\n");
+        exit(-1);
+    }
+
+    close(client_socket);
+
+    return 0;
+}
