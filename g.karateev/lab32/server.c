6ea8ffa91381a0a603502ad1c710d1c4aabf6b57
Georgii Karateev <g.karateev@g.nsu.ru>
2024-12-10 11:53:22 +0700

    lab32 fix

--- g.karateev/lab32/server.c
+++ g.karateev/lab32/server.c
@@ -2,145 +2,119 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <ctype.h>
-#include <errno.h>
+#include <pthread.h>
 
 #define SOCKET_PATH "./unix_socket"
 #define BUFFER_SIZE 1024
 #define BACKLOG 5
 #define MAX_CLIENTS 10
 
-int server_fd;
-int client_fds[MAX_CLIENTS] = {0};
+int client_counter = 0;
+int active_clients = 0; // Количество активных клиентов
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void to_uppercase(char *str) {
-    for (int i = 0; str[i]; i++) {
-        str[i] = toupper((unsigned char) str[i]);
-    }
-}
-
-void handle_sigio(int sig) {
+void *handle_client(void *arg) {
+    int client_fd = *(int *)arg;
+    int client_id;
     char buffer[BUFFER_SIZE];
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_fds[i] > 0) {
-            ssize_t bytes_read = read(client_fds[i], buffer, sizeof(buffer) - 1);
-            if (bytes_read > 0) {
-                buffer[bytes_read] = '\0';
-                to_uppercase(buffer);
-                printf("Client %d: %s\n", client_fds[i], buffer);
-            } else if (bytes_read == 0) {
-                printf("Client %d disconnected\n", client_fds[i]);
-                close(client_fds[i]);
-                client_fds[i] = 0;
-            } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
-                perror("read");
-                close(client_fds[i]);
-                client_fds[i] = 0;
-            }
-        }
-    }
-}
+    ssize_t bytes_received;
 
-void add_client(int client_fd) {
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (client_fds[i] == 0) {
-            client_fds[i] = client_fd;
-            return;
+    // Увеличиваем счетчик клиентов и присваиваем ID текущему клиенту
+    pthread_mutex_lock(&counter_mutex);
+    client_id = ++client_counter;
+    active_clients++;
+    pthread_mutex_unlock(&counter_mutex);
+
+    printf("Client %d connected\n", client_id);
+
+    while ((bytes_received = read(client_fd, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[bytes_received] = '\0';
+
+        // Преобразуем текст в верхний регистр
+        for (int j = 0; buffer[j] != '\0'; j++) {
+            buffer[j] = toupper(buffer[j]);
         }
+
+        printf("Client %d: %s\n", client_id, buffer);
     }
-    fprintf(stderr, "Too much clients!\n");
+
+    printf("Client %d disconnected\n", client_id);
+
+    // Уменьшаем количество активных клиентов
+    pthread_mutex_lock(&counter_mutex);
+    active_clients--;
+    int should_exit = (active_clients == 0); // Проверяем, нужно ли завершать сервер
+    pthread_mutex_unlock(&counter_mutex);
+
     close(client_fd);
-}
+    free(arg);
 
-void set_nonblocking(int fd) {
-    int flags = fcntl(fd, F_GETFL, 0);
-    if (flags == -1) {
-        perror("fcntl F_GETFL");
-        exit(EXIT_FAILURE);
+    // Если активных клиентов нет, завершаем сервер
+    if (should_exit) {
+        printf("No more clients. Server shutting down.\n");
+        exit(0);
     }
-    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-        perror("fcntl F_SETFL O_NONBLOCK");
-        exit(EXIT_FAILURE);
-    }
+
+    return NULL;
 }
 
 int main() {
-    struct sockaddr_un addr;
+    int server_fd, client_fd;
+    struct sockaddr_un address;
+    struct sockaddr_un client_address;
+    socklen_t client_len;
+    pthread_t client_thread;
 
-    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("\nSocket creation error\n");
+        exit(1);
     }
 
     unlink(SOCKET_PATH);
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
 
-    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-        perror("bind");
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("\nServer bind error\n");
         close(server_fd);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     if (listen(server_fd, BACKLOG) == -1) {
-        perror("listen");
+        perror("\nServer listen error\n");
         close(server_fd);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    // Настраиваем серверный сокет для неблокирующего режима
-    set_nonblocking(server_fd);
-
-    if (fcntl(server_fd, F_SETOWN, getpid()) == -1) {
-        perror("fcntl F_SETOWN");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-    if (fcntl(server_fd, F_SETFL, O_NONBLOCK | O_ASYNC) == -1) {
-        perror("fcntl F_SETFL O_ASYNC");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_sigio;
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGIO, &sa, NULL) == -1) {
-        perror("sigaction");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server startes and listening on: %s\n", SOCKET_PATH);
+    printf("Server started and listening on %s\n", SOCKET_PATH);
 
     while (1) {
-        int client_fd = accept(server_fd, NULL, NULL);
+        client_len = sizeof(client_address);
+        client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_len);
         if (client_fd == -1) {
-            if (errno != EAGAIN && errno != EWOULDBLOCK) {
-                perror("accept");
-            }
-        } else {
-            printf("Client connected: %d\n", client_fd);
-            set_nonblocking(client_fd);
-            if (fcntl(client_fd, F_SETOWN, getpid()) == -1) {
-                perror("fcntl F_SETOWN");
-            }
-            if (fcntl(client_fd, F_SETFL, O_NONBLOCK | O_ASYNC) == -1) {
-                perror("fcntl F_SETFL O_ASYNC");
-            }
-            add_client(client_fd);
+            perror("\nAccept error\n");
+            continue;
         }
 
+        int *client_fd_ptr = malloc(sizeof(int));
+        *client_fd_ptr = client_fd;
+
+        if (pthread_create(&client_thread, NULL, handle_client, client_fd_ptr) != 0) {
+            perror("\nThread creation error\n");
+            close(client_fd);
+            free(client_fd_ptr);
+        } else {
+            pthread_detach(client_thread);
+        }
     }
 
     close(server_fd);
     unlink(SOCKET_PATH);
+
     return 0;
 }
